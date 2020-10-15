a7a670fe10c4de856ba0c00f97ca7357ea0b93c0
Walter C <walter.condori.10@gmail.com>
2020-10-15 01:11:01 -0700

    Favorite Tweet Feature Added

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -30,6 +30,8 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -107,6 +109,9 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         
         return cell
     }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,6 +13,45 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Favorite Did Not Succeed \(Error)")
+            })
+        }else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("Unfavorite Did Not Succeed \(Error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        
+    }
+    
+    //var favorited:Bool = false
+    //var tweetId:Int = -1
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()

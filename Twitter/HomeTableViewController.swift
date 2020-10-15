2d2e1b810ac26cffc00ab7229e9a7f21a2b23cf3
Walter C <walter.condori.10@gmail.com>
2020-10-14 23:49:29 -0700

    Posting Tweet Feature Added

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -25,12 +25,16 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello World")
         
-        loadTweets()
+        //loadTweets()
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets(){
